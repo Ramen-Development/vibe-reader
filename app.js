3b21a4e9c66d6a57bb340b58bc2fd2b6d982edd6
vandelvan <vandel_van@hotmail.com>
2022-12-04 17:36:18 -0600

    different port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 const router = express.Router();
-const port = 3000;
+const port = 8080;
 
 app.use(express.static("public"));
 
