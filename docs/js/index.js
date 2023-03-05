4b20e4e32b38718594eee34514872802a1a4cda1
Gustavo P <piloalucard@gmail.com>
2023-03-04 19:27:56 -0600

    #6 #7 Context questions, reactions to answers, help line number

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,17 @@
 //test env:
-// const apiurl = "http://localhost:3000/";
+const apiurl = "http://localhost:3000/";
 //prod env:
-const apiurl = "https://vibe-readerapi.onrender.com/";
+//const apiurl = "https://vibe-readerapi.onrender.com/";
+
+const contextQuestions = [
+  "¿Por qué te sientes así?",
+  "¿Qué te hace sentir así?",
+  "¿Lo cambiarias si pudieras?",
+  "¿Alguien influyó en lo que me comentas?",
+];
+
+
+
 const emotions_en = [
   "Admiration",
   "Amusement",
@@ -65,6 +75,22 @@ const emotions = [
 ];
 
 const questions_en = [
+  "How are you feeling ?",
+  "What is bothering you the most at the moment?",
+  "Has there been anything that has made you feel better lately?",
+  "Would you like to talk about what is bothering you or do you feel more comfortable keeping it to yourself?", "Have you talked to anyone else about how you are feeling?", "Have you talked to anyone else about how you are feeling?",
+  "Have you talked to anyone else about how you are feeling?",
+  "How do you spend your free time?",
+  "Do you have any hobbies or interests that you are passionate about?",
+  "Do you have any books, movies, or series that you would recommend?",
+  
+  "Do you have any dreams or goals you would like to achieve in the future?", "Do you have any dreams or goals you would like to achieve in the future?",
+  "How would you describe your personality?",
+  "What do you like most about yourself?",
+  "What kind of food do you like to eat?",
+  "How would you describe yourself in stressful situations?",
+  "Is there anything that life has taught you that you always remember?",
+
   "What you do when you feel happy?",
   "What do you do when you are alone?",
   "What is your purpose in life?",
@@ -100,6 +126,22 @@ const questions_en = [
 ];
 
 const questions = [
+  "¿Cómo te sientes hoy?",
+  "¿Qué es lo que más te está preocupando en este momento?",
+  "¿Ha habido algo que te haya hecho sentir mejor últimamente?",
+  "¿Te gustaría hablar sobre lo que te está molestando o te sientes más cómodo/a guardándolo para ti?",
+  "¿Has hablado con alguien más sobre cómo te sientes?",
+  "¿Cómo pasas tu tiempo libre?",
+  "¿Tienes algún hobby o interés que te apasiona?",
+  "¿Tienes algún libro, película o serie que recomendarías?",
+  
+  "¿Tienes algún sueño o meta que te gustaría alcanzar en el futuro?",
+  "¿Cómo describirías tu personalidad?",
+  "¿Qué es lo que más te gusta de ti?",
+  "¿Qué tipo de comida te gusta comer?",
+  "¿Cómo te describirías en situaciones de estrés?",
+  "¿Hay algo que te haya enseñado la vida que siempre recuerdas?",
+
   "¿Que haces cuando te sientes feliz?",
   "¿Que haces cuando estas solo?",
   "¿Cual es tu proposito en la vida?",
@@ -135,6 +177,7 @@ const questions = [
 
 const questionsNumber = questions.length;
 
+
 let emotionsResults = new Array(28).fill(0);
 
 let usedQuestions = new Array(questionsNumber).fill(0);
@@ -143,7 +186,9 @@ function setText(text) {
   document.getElementById("status").innerText = text;
 }
 
+let newQuestionNeeded = true;
 let neededmsgs = 0;
+let helpMsg = false;
 async function userMsg() {
   let msgInput = document.getElementById("message");
   let msg = msgInput.value;
@@ -160,9 +205,10 @@ async function userMsg() {
   });
   let id = await evaluation.json();
   id = id.id;
-  console.log(id);
   emotionsResults[id] = emotionsResults[id] + 1;
   neededmsgs++;
+
+  
 
   iterator = -1;
   document.getElementById("botface").innerHTML =
@@ -173,24 +219,111 @@ async function userMsg() {
     //Results
     for (let i = 0; i < emotionsResults.length; i++) {
       if (emotionsResults[i] > 0) {
+        
         showResult(emotions[i], emotionsResults[i].toString());
+        if (emotions[i] == "Tristeza" && emotionsResults[i] >= 2) {
+          helpMsg = true;
+        }
       }
     }
+
+    if (helpMsg) {
+      showHelpMsg();
+
+    }
+
     //Borrar boton
     let interaction = document.getElementById("interact");
     interaction.remove();
   } else {
     let iaux = 0;
-    while (iaux == 0) {
-      let aux = Math.floor(Math.random() * questionsNumber);
-      if (usedQuestions[aux] == 0) {
-        botMsg(questions[aux]);
-        usedQuestions[aux]++;
-        iaux = 1;
+    
+    if(botReaction(emotions[id]) && !newQuestionNeeded){
+      let aux = Math.floor(Math.random() * contextQuestions.length);
+      botMsg(contextQuestions[aux]);
+      newQuestionNeeded = true;
+    }
+    else{
+      while (iaux == 0) {
+        let aux = Math.floor(Math.random() * questionsNumber);
+        if (usedQuestions[aux] == 0) {
+          botMsg(questions[aux]);
+          usedQuestions[aux]++;
+          iaux = 1;
+        }
       }
+      newQuestionNeeded = false;
     }
   }
 }
+
+function botReaction(emotion){
+  let rand = Math.floor(Math.random() * 3);
+  console.log(emotion);
+
+  switch(emotion){
+
+    case "Alegría":
+      botMsg("Detecto "+emotion);
+      if(rand == 0){
+        botMsg("Me alegra que te sientas asi");
+      }else if(rand == 1){
+        botMsg("Que bueno que te sientas asi");
+      }else{
+        botMsg("Muy bien");
+      }
+      break;
+    case "Tristeza":
+      botMsg("Detecto "+emotion);
+      if(rand == 0){
+        botMsg("Todo va a estar bien");
+      }else if(rand == 1){
+        botMsg("No te preocupes, todo va a estar bien");
+      }else{
+        botMsg("No estas solo.");
+      }
+      break;
+    case "Neutral":
+      if(rand == 0){
+        botMsg("Entiendo");
+      }else{
+        botMsg("Okay");
+      }
+      return false;
+
+    case "Enojo": case "Molestia":
+      botMsg("Detecto "+emotion);
+      if(rand == 0){
+        botMsg("Es comprensible que te sientas asi");
+      }else{
+        botMsg("Tranquilo, no pasa nada");
+      }
+      break;
+    case "Miedo":
+      botMsg("Detecto "+emotion);
+      if(rand == 0){
+        botMsg("");
+      }else{
+        botMsg("Estas seguro, tranquilo");
+      }
+      break;
+    default:
+      return false;
+  }
+  return true;
+
+
+}
+
+function showHelpMsg(){
+  document.getElementById("chat").innerHTML += "<br>";
+
+  botMsg("Es probable que necesites ayuda en base a tus respuestas. Te recomendamos que busques una opinión profesional.");
+  botMsg("(México) Línea de la vida: 800 911 2000");
+  botMsg("(México) Instituto Nacional de Psiquiatría: 55 4160 3282");
+  botMsg("Animo, todo va a estar bien.");
+}      
+
 
 function botMsg(msg) {
   document.getElementById("chat").innerHTML +=
@@ -209,7 +342,7 @@ function showResult(msg, val) {
     val +
     '" max="10">' +
     val +
-    "/9 </progress>";
+    "/9 </progress> ";
 }
 
 let aux = Math.floor(Math.random() * questionsNumber);
